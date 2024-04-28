b96c9b99909612a853b605bdfab2e55b1e489aa8
Akmaliin <akmalnafis25@gmail.com>
2024-04-28 14:14:14 +0700

    [TASK 0] Base Project

---
+++ app/src/main/java/com/cryptoin/nolancrypto/data/mapper/CoinDetailMapper.kt
@@ -0,0 +1,19 @@
+package com.cryptoin.nolancrypto.data.mapper
+
+import com.cryptoin.nolancrypto.data.model.CoinDetail
+import com.cryptoin.nolancrypto.data.source.network.model.coindetail.CoinDetailResponse
+
+
+fun CoinDetailResponse.toCoinDetail() =
+    CoinDetail(
+        id = this.id.orEmpty(),
+        symbol = this.symbol.orEmpty(),
+        name = this.name.orEmpty(),
+        image = this.image?.large.orEmpty(),
+        currentPrice = this.currentPrice.orEmpty(),
+        description = this.description?.en.orEmpty()
+    )
+
+fun Collection<CoinDetailResponse>.toCoinDetails() = this.map {
+    it.toCoinDetail()
+}
