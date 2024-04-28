03d600fa9b467bd5091a30a85354414131b3e2df
Akmaliin <akmalnafis25@gmail.com>
2024-04-28 14:17:35 +0700

    [TASK 0] Base Project

--- app/src/main/java/com/cryptoin/nolancrypto/presentation/main/MainActivity.kt
+++ app/src/main/java/com/cryptoin/nolancrypto/presentation/main/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.cryptoin.nolancrypto.presentation.main
 
-import android.content.Intent
+
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
+
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cryptoin.nolancrypto.R
