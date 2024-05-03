432d92604e27249e4b57c632b6b1b901aecd78aa
hassanali <dongbinil357@gmial.com>
2024-05-03 18:03:42 +0700

    add market

--- app/src/main/java/com/cryptoin/nolancrypto/presentation/main/MainActivity.kt
+++ app/src/main/java/com/cryptoin/nolancrypto/presentation/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cryptoin.nolancrypto.presentation.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -16,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +29,22 @@ class MainActivity : AppCompatActivity() {
     private fun setupBottomNav() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.navView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            when (destination.id) {
+        binding.navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.menu_tab_profile -> {
                     if (!viewModel.isLoggedIn()) {
                         navigateToLogin()
-                        controller.popBackStack(R.id.menu_tab_home, false)
+                        navController.popBackStack(R.id.menu_tab_home, false)
+                        return@setOnItemSelectedListener false
                     }
                 }
+                R.id.market_icon -> {
+                    Toast.makeText(this, "Under Maintenance", Toast.LENGTH_SHORT).show()
+                    return@setOnItemSelectedListener false
+                }
             }
+            navController.navigate(item.itemId)
+            true
         }
     }
 
@@ -45,18 +52,12 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(this, LoginActivity::class.java))
     }
 
-//    fun navigateToTabProfile() {
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        binding.navView.selectedItemId = R.id.menu_tab_profile
-//        navController.navigate(R.id.menu_tab_profile)
-//    }
-
     private fun checkFirstRun() {
         val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val isFirstRun = sharedPreferences.getBoolean("isFirstRun", true)
 
         // delete negasi, Intro just showing at the first time run
-        if (!isFirstRun) {
+        if (isFirstRun) {
             startActivity(Intent(this, MyAppIntroActivity::class.java))
             // Set isFirstRun to false to indicate that the app has been launched before
             sharedPreferences.edit().putBoolean("isFirstRun", false).apply()
